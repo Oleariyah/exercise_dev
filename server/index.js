287d41531582d7d4f8835304627a15542c8ab826
oleariyah <oleariyah@gmail.com>
2018-10-24 17:45:11 +0100

    Re-structure the projects and updated the css

--- server/index.js
+++ server/index.js
@@ -1,59 +1,12 @@
-const { GraphQLServer } = require('graphql-yoga')
+const { GraphQLServer } = require('graphql-yoga');
 const mongoose = require('mongoose');
+const resolvers = require ('./resolver/resolvers');
+const typeDefs = require('./schema/types');
 
 //connect to MongoDB with mongoose
 mongoose.connect('mongodb://localhost/test');
 
-//database schema
-const Todo = mongoose.model('Todo', {
-    text: String,
-    complete: Boolean
-})
-
-//graphqlserver schema
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-    todos: [Todo]
-  }
-
-  type Todo {
-      id: ID!
-      text: String!
-      complete: Boolean!
-  }
-
-  type Mutation {
-    createTodo(text: String!) : Todo
-    updateTodo(id: ID!, complete: Boolean!) : Boolean
-    removeTodo(id: ID!) : Boolean 
-}
-`
-const resolvers = {
-  Query: {
-    hello: (_, { name }) => `Hello ${name || 'World'}`,
-    todos: () => Todo.find()
-  },
-    Mutation: {
-        createTodo: async (_, {text}) => {
-            const todo = new Todo({text, complete:false});
-            await todo.save();
-            return todo;
-            },
-
-        updateTodo: async (_, {id, complete}) => {
-            await Todo.findByIdAndUpdate(id, {complete});
-            return true;
-        },
-       
-        removeTodo: async (_, {id}) => {
-            await Todo.findByIdAndRemove(id);
-            return true;
-        }
-
-    }
-}
-
+//connect to the database and run the server
 const server = new GraphQLServer({ typeDefs, resolvers })
 mongoose.connection.once('open', () => 
 server.start(() => console.log('Server is running on localhost:4000')));
